744299975713a18b00f9374e078da4ba180252e8
olekturbo <aleks.szewczak@gmail.com>
2019-02-26 20:17:42 +0100

    Finalizacja laboratorium 2 - komunikator

--- lab2/zad1/klient.c
+++ lab2/zad1/klient.c
@@ -1,49 +1,63 @@
+#include <fcntl.h> 
+#include <unistd.h> 
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#define DLUGOSC_WIADOMOSCI 256
+#define UPRAWNIENIA 0711
+
+void sprawdzDostepnosc() {	
+	while (open("lockfile",O_CREAT|O_EXCL,0)==-1) {
+       printf("Serwer zajety, prosze czekac\n\n");	
+       sleep(1);
+    }
+}
 
 void wyczyscBufor() {
-	int wyniki = open("wyniki.txt", O_RDWR, O_CREAT);
-	char czyszczenie[1] = "";
-	write(wyniki, czyszczenie, 1);
-	close(wyniki);
+	unlink("wyniki.txt");
 }
 
 void czytaj() {
-	int dane = open("dane.txt", O_RDWR, O_CREAT);
-	char odpowiedz[256] = "";
-	read(dane,odpowiedz,256);
-	close(dane);
-	printf("%s", odpowiedz);
-}
-
-void pisz() {
-	int wyniki = open("wyniki.txt", O_RDWR, O_CREAT);
-	char pytanie[256] = "";
-	int i = 0;
-	char c;
-	printf("Zadaj pytanie:\n");
-	while((c = getchar())  != 27) {
-		pytanie[i] = c;
-		i++;
-	}
-	write(wyniki, pytanie, 256);
-	close(wyniki);
-}
-
-void sprawdzDostepnosc() {
-	while (open("lockfile",O_CREAT|O_EXCL,0)==-1) {
-      printf("Serwer zajety, prosze czekac\n");
-      sleep(1);
+   int wyniki = 0;
+   char odpowiedz[DLUGOSC_WIADOMOSCI] = "";
+ 
+   while((wyniki = open("wyniki.txt", O_RDWR, UPRAWNIENIA)) == -1) {}
+	
+   if(wyniki != 0) {
+		while(read(wyniki,odpowiedz,DLUGOSC_WIADOMOSCI) == 0) {}	
+		printf("ODPOWIEDZ SERWERA!\n");
+		printf("Serwer: %s" , odpowiedz);
    }
 }
 
-int main(int argc, char *argv[]) {
-	wyczyscBufor();
-	pisz();
-	sprawdzDostepnosc();
-	czytaj();
+void pisz() {
+	int dane;
+	char pytanie[DLUGOSC_WIADOMOSCI] = "";
+	char c;
+	int i;
 	
+	while(1) {
+		dane = open("dane.txt", O_RDWR|O_CREAT, UPRAWNIENIA);
+		if(dane != 0) {
+			i = 0;
+			printf("\nPytanie klienta:\n");
+				
+			while((c = getchar()) != 27) {
+				pytanie[i] = c;
+				i++;
+			}
+				
+			write(dane,pytanie,i);
+			close(dane);
+			break;
+		}
+	}
 }
 
+int main() {
+	
+	sprawdzDostepnosc();
+	pisz();
+	czytaj();
+	wyczyscBufor();
+
+    return 0;
+}
--- lab2/zad1/serwer.c
+++ lab2/zad1/serwer.c
@@ -1,29 +1,63 @@
+#include <fcntl.h> 
+#include <unistd.h> 
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#define DLUGOSC_WIADOMOSCI 256
+#define UPRAWNIENIA 0711
 
-int main(){
-
-  int wyniki = open("wyniki.txt", O_RDWR, O_CREAT);
-  int dane = open("dane.txt", O_RDWR, O_CREAT);
-  char pytanie[256] = "";
-  char odpowiedz[256] = "";
-  char czyszczenie[1] = "";
-  
-  while(1) {
-	  if(read(wyniki,pytanie,256) != NULL) {
-		  printf("Pytanie: %s\n", pytanie);
-		  printf("Podaj odpowiedz:\n");
-		  scanf("%s", odpowiedz);
-		  write(dane, "Odpowiedz: ", 11);
-		  write(dane, odpowiedz, 256);
-		  printf("\nLockfile usuniety\n");
-		  unlink("lockfile"); 
-		  write(wyniki,czyszczenie,1);
-		  close(wyniki);
-	  }
-  }
-  close(wyniki);
+void wyczyscBufor() {
+		unlink("lockfile");
+		unlink("dane.txt");
 }
+
+void czytaj() {
+   int dane = 0;
+   char pytanie[DLUGOSC_WIADOMOSCI] = "";
+   char uzytkownik[64];
+   getlogin_r(uzytkownik, sizeof(uzytkownik)-1);
+ 
+   while((dane = open("dane.txt", O_RDWR, UPRAWNIENIA)) == -1) {}
+	
+   if(dane != 0)
+   {
+		while(read(dane,pytanie,DLUGOSC_WIADOMOSCI) == 0) {}	
+		printf("NOWA WIADOMOSC!\n");
+		printf("Klient (%s): %s" , uzytkownik, pytanie);
+	}
+}
+
+void pisz() {
+	int wyniki;
+	char odpowiedz[DLUGOSC_WIADOMOSCI] = "";
+	char c;
+	int i;
+	
+	while(1) {
+		wyniki = open("wyniki.txt", O_RDWR|O_CREAT, UPRAWNIENIA);
+		if(wyniki != 0) {
+			i = 0;
+			printf("\nOdpowiedz serwera:\n");
+				
+			while((c = getchar()) != 27) {
+				odpowiedz[i] = c;
+				i++;
+			}
+				
+			write(wyniki,odpowiedz,i);
+			close(wyniki);
+			break;
+		}
+	}
+}
+
+int main() {	
+	
+	printf("### START SERWERA ###\n");
+	while(1) {
+		czytaj();
+		pisz();
+		wyczyscBufor();
+	}
+		
+	return 0;
+}
+
